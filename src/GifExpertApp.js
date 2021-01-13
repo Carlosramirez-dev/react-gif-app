c786cb0908c27960f68b57706e042258cfb5c31b
Carlos Ramirez <carlosramirezdev@gmail.com>
2021-01-13 12:07:32 -0600

    Primer commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import GifGrid from './components/GifGrid';
+
+
+function GifExpertApp() {
+
+    const [categories, setCategories] = useState(['Dragon Ball Z']);
+
+    // const handleAdd = () => {
+
+    //     // setCategories(['New category', ...categories]);
+    //     setCategories( cat => [...categories, 'New category']);
+    // }
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={ setCategories } />
+            <hr/>
+
+            <ol>
+                { 
+                    categories.map( category => (
+                        <GifGrid 
+                            key={ category }
+                            category={ category }
+                        />
+                    ))
+                }
+            </ol>
+        </>
+    );
+}
+
+export default GifExpertApp;
